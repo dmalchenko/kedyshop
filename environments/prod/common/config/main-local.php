e48958c928cac9a49c4a2b1eba681ba9913ae7ba
dmalchenko <malchenko.dmitriy.by@gmail.com>
2018-06-20 20:13:06 +0300

    init

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=kedyshop',
+            'username' => 'postgres',
+            'password' => 'postgres',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
