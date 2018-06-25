a98136ae519ebdfcf1995e5640d507c084cfb999
Nickolay <np@topdog.ru.net>
2018-06-25 13:51:27 +0300

    Added css-structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
+var autoprefixer = require('autoprefixer');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
 
 gulp.task('css', function(){
   return gulp.src('frontend/assets/stylesheets/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass(({outputStyle: 'compressed'})).on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('frontend/web/css/'))
 });
