a32ee3a77f1a71d9a614d1e8bcfd0929a84cb993
Nickolay <np@topdog.ru.net>
2018-06-26 20:45:52 +0300

    Catalogue item

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,7 +11,9 @@ $this->title = 'My Yii Application';
                 <li class="catalogue-content__list-item">
                     <div class="catalogue-item">
                         <a class="catalogue-item__title" href="">CONVERSE CHUCK TAYLOR ALL STAR II OX Grey (CONVERSE-89)</a>
-                        <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        <a class="catalogue-item__img-link" href="">
+                            <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        </a>
                         <select class="catalogue-item__size">
                             <option>36 euro</option>
                             <option>37 euro</option>
@@ -32,7 +34,9 @@ $this->title = 'My Yii Application';
                 <li class="catalogue-content__list-item">
                     <div class="catalogue-item">
                         <a class="catalogue-item__title" href="">CONVERSE CHUCK TAYLOR ALL STAR II OX Grey (CONVERSE-89)</a>
-                        <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        <a class="catalogue-item__img-link" href="">
+                            <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        </a>
                         <select class="catalogue-item__size">
                             <option>36 euro</option>
                             <option>37 euro</option>
@@ -44,7 +48,7 @@ $this->title = 'My Yii Application';
                         </select>
                         <span class="catalogue-item__price catalogue-item__price--old">Старая цена: 5990<span class="ruble">Р</span></span>
                         <span class="catalogue-item__price">Новая цена: 2990<span class="ruble">Р</span></span>
-                        <div class="catalogue-item__btns btns">
+                        <div class="catalogue-item__btns btns btns--spaced">
                             <a class="btn btn--secondary" href="">Подробнее</a>
                             <a class="btn" href="">Заказать</a>
                         </div>
@@ -53,7 +57,9 @@ $this->title = 'My Yii Application';
                 <li class="catalogue-content__list-item">
                     <div class="catalogue-item">
                         <a class="catalogue-item__title" href="">CONVERSE CHUCK TAYLOR ALL STAR II OX Grey (CONVERSE-89)</a>
-                        <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        <a class="catalogue-item__img-link" href="">
+                            <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        </a>
                         <select class="catalogue-item__size">
                             <option>36 euro</option>
                             <option>37 euro</option>
@@ -65,13 +71,76 @@ $this->title = 'My Yii Application';
                         </select>
                         <span class="catalogue-item__price catalogue-item__price--old">Старая цена: 5990<span class="ruble">Р</span></span>
                         <span class="catalogue-item__price">Новая цена: 2990<span class="ruble">Р</span></span>
-                        <div class="catalogue-item__btns btns">
+                        <div class="catalogue-item__btns btns btns--spaced">
+                            <a class="btn btn--secondary" href="">Подробнее</a>
+                            <a class="btn" href="">Заказать</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="catalogue-content__list-item">
+                    <div class="catalogue-item">
+                        <a class="catalogue-item__title" href="">CONVERSE CHUCK TAYLOR ALL STAR II OX Grey (CONVERSE-89)</a>
+                        <a class="catalogue-item__img-link" href="">
+                            <img class="catalogue-item__img" src="images/content/content-7.jpg" alt="">
+                        </a>
+                        <select class="catalogue-item__size">
+                            <option>36 euro</option>
+                            <option>37 euro</option>
+                            <option>38 euro</option>
+                            <option>39 euro</option>
+                            <option>40 euro</option>
+                            <option>41 euro</option>
+                            <option>42 euro</option>
+                        </select>
+                        <span class="catalogue-item__price catalogue-item__price--old">Старая цена: 5990<span class="ruble">Р</span></span>
+                        <span class="catalogue-item__price">Новая цена: 2990<span class="ruble">Р</span></span>
+                        <div class="catalogue-item__btns btns btns--spaced">
                             <a class="btn btn--secondary" href="">Подробнее</a>
                             <a class="btn" href="">Заказать</a>
                         </div>
                     </div>
                 </li>
             </ul>
+            <div class="catalogue-content__desc">
+                <h2>Интернет-магазин кроссовок в Москве</h2>
+
+
+                <p>Кроссовки давно перестали считаться исключительно спортивной обувью. Стильный внешний вид, долговечность, а также комфорт во время носки позволили им завоевать симпатии людей самых разных возрастных категорий, проживающих на шести континентах.</p>
+
+                <p>Говоря о качественных кроссовках, мы имеем в виду продукцию брендов с мировым именем, таких как Puma, Nike, Asics, New Balance, Adidas, Reebok, FILA и других. Данные компании используют передовые технологии, разрабатывая новые модели. Широкая линейка продукции способна удовлетворить запросы даже самых требовательных клиентов.</p>
+
+                <p>Однако, как и всегда, у медали две стороны. Люди, отдающие предпочтение высококачественной продукции, вынуждены платить непомерные деньги за бренд.</p>
+
+                <p>Часто цена пары сильно завышена и не соответствует действительности. Стоимость кроссовок формируется с учетом сразу нескольких факторов. Успешная работа маркетингового отдела – далеко не последний из них.</p>
+
+                <p>Сложившаяся ситуация вынуждает потребителей, заинтересованных в приобретении хорошей обуви, искать другие, более лояльные и выгодные варианты совершения покупки.</p>
+
+                <p>StokerMan – первопроходец отечественного рынка оптовых поставок брендовых кроссовок. Мы работаем с проверенными производителями, а также используем уникальную технологию доставки, складирования и реализации продукции. Это позволяет нам гарантировать максимально выгодные условия сотрудничества – как для оптовых, так и для розничных покупателей.</p>
+
+                <p>Как купить модные кроссовки и кеды
+                    Если Вы хотите приобрести стильную городскую обувь из последних коллекций ведущих мировых брендов, доверьтесь нам. Интернет-магазин StokerMan готов предложить низкие цены, уникальный ассортимент, а также сезонные скидки. Все это позволит обзавестись стильной и долговечной обувью, которая проходит сразу несколько этапов контроля качества.</p>
+
+                <p>Изготовленные с применением передовых технологий и прочных материалов, а также соблюдением европейских нормативов, кроссовки обладают отечественной ценой, ведь производителю не нужно содержать большой штат маркетологов и тратиться на продвижение продукции на рынке.</p>
+
+                <p>Вся обувь, представленная в обширном электроном каталоге интернет-магазина StokerMan, имеется на складе. Именно поэтому Вы можете рассчитывать на быструю адресную доставку.</p>
+
+                <p>Обоснованные цены дополняются выгодными программами лояльности. 10% скидки постоянным клиентам, бесплатная доставка при заказе от 5 000 рублей, а также возможность совершить обмен/возврат неношеной обуви – несомненные преимущества совершения покупки онлайн.</p>
+
+                <p>Предложение оптовикам
+                    Мы работаем по всей России - сотрудничаем как с крупными интернет-магазинами, так и с небольшими торговыми точками. Применяя дропшопинг, мы позволяем нашим оптовым клиентам полностью исключить логистику из своих бизнес-процессов. Доверив поставки нам, Вы также можете быть уверены в лояльности и привлекательности цен. Мы не пользуемся услугами подрядчиков, что позволяет:</p>
+
+                <p>а) гарантировать отсутствие наценок;</p>
+
+                <p>б) проводить весь цикл работ – от закупок товаров и обработки заказов до доставки продукции клиенту.</p>
+
+                <p>У Вас остались вопросы? Не можете определиться с выбором? Обращайтесь к нам любым удобным образом:</p>
+
+                <p>позвонив по номеру телефона +7 (499) 110-99-71;
+                связавшись с онлайн-консультантом;
+                заказав обратный звонок с помощью специальной формы, оборудованной внизу страницы;
+                добавив товар в корзину и приступив к оформлению заказа.
+                    Купить стильные брендовые кроссовки легко. Убедитесь в этом сами, воспользовавшись помощью интернет-магазина StokerMan.</p>
+            </div>
         </div>
         <div class="catalogue-sidebar">123</div>
     </div>
