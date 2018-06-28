74a123e01b3cf16823105ad47010d99d538be4d0
dmalchenko <malchenko.dmitriy.by@gmail.com>
2018-06-28 20:01:33 +0300

    add purchase method

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -14,6 +14,7 @@ use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
+use yii\web\Response;
 
 /**
  * Site controller
@@ -236,5 +237,12 @@ class SiteController extends Controller
     public function actionDelivery()
     {
         return $this->render('delivery');
+    }
+
+    public function actionPurchase()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        return ['status' => 'OK', 'code' => 200, 'data' => []];
     }
 }
