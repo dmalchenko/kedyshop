0183350ffbd8e481d728d86d47b1b62483749971
Nick Prokopenko <drnickopol@gmail.com>
2018-06-29 01:52:48 +0300

    Added product page

--- frontend/views/site/item.php
+++ frontend/views/site/item.php
@@ -4,32 +4,56 @@
 /* @var $item \common\models\Item */
 ?>
 
-
-<li class="catalogue-content__list-item">
-    <div class="catalogue-item" data-id="<?= $item->id ?>">
-        <span style="color: #9fa7aa; font-size: 10px; margin: 0 0 10px 0">Артикул <?= $item->article ?></span>
-        <a class="catalogue-item__title" href="<?= \yii\helpers\Url::toRoute(['site/item', 'id' => $item->id])?>"><?= $item->title ?></a>
-        <a class="catalogue-item__img-link" href="<?= \yii\helpers\Url::toRoute(['site/item', 'id' => $item->id])?>">
-            <img class="catalogue-item__img" src="<?= $item->imageUrl ?>" alt="">
-        </a>
-        <select class="catalogue-item__size">
-            <option>36 euro</option>
-            <option>37 euro</option>
-            <option>38 euro</option>
-            <option>39 euro</option>
-            <option>40 euro</option>
-            <option>41 euro</option>
-            <option>42 euro</option>
-        </select>
-        <?php
-        if ($item->old_price) {
-            echo sprintf('<span class="catalogue-item__price catalogue-item__price--old">Старая цена: %s<span class="ruble">Р</span></span>', $item->old_price);
-        }
-        ?>
-
-        <span class="catalogue-item__price">Цена: <?= $item->new_price ?><span class="ruble">Р</span></span>
-        <div class="catalogue-item__btns btns btns--spaced">
-            <a class="btn" href="">Купить</a>
+<div class="product-page">
+    <div class="product-page__row">
+        <div class="product-page__product">
+            <div class="product" data-id="<?= $item->id ?>">
+                <a class="product__title" href="<?= \yii\helpers\Url::toRoute(['site/item', 'id' => $item->id])?>"><?= $item->title ?></a>
+                <span class="product__articul">Артикул <?= $item->article ?></span>
+                <a class="product__img-link" href="<?= \yii\helpers\Url::toRoute(['site/item', 'id' => $item->id])?>">
+                    <img class="product__img" src="<?= $item->imageUrl ?>" alt="">
+                </a>
+                <div class="product__info">
+                    <div class="product__promo">
+                        <div class="product__promo-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 26 26" enable-background="new 0 0 26 26">
+                                <path d="m.3,14c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1v-8.88178e-16c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.4 0.4,1 0,1.4l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.5-0.1-0.7-0.3l-7.8-8.4-.2-.3z"/>
+                            </svg>
+                            <span>100% ОРИГИНАЛЬНАЯ ПРОДУКЦИЯ</span>
+                        </div>
+                        <div class="product__promo-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 26 26" enable-background="new 0 0 26 26">
+                                <path d="m.3,14c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1v-8.88178e-16c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.4 0.4,1 0,1.4l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.5-0.1-0.7-0.3l-7.8-8.4-.2-.3z"/>
+                            </svg>
+                            <span>ОФИЦИАЛЬНАЯ ПОСТАВКА В РОССИЮ (РСТ)</span>
+                        </div>
+                        <div class="product__promo-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 26 26" enable-background="new 0 0 26 26">
+                                <path d="m.3,14c-0.2-0.2-0.3-0.5-0.3-0.7s0.1-0.5 0.3-0.7l1.4-1.4c0.4-0.4 1-0.4 1.4,0l.1,.1 5.5,5.9c0.2,0.2 0.5,0.2 0.7,0l13.4-13.9h0.1v-8.88178e-16c0.4-0.4 1-0.4 1.4,0l1.4,1.4c0.4,0.4 0.4,1 0,1.4l0,0-16,16.6c-0.2,0.2-0.4,0.3-0.7,0.3-0.3,0-0.5-0.1-0.7-0.3l-7.8-8.4-.2-.3z"/>
+                            </svg>
+                            <span>ТОВАР В НАЛИЧИИ</span>
+                        </div>
+                    </div>
+                    <div class="product__desc">
+                        <?php
+                        if ($item->old_price) {
+                            echo sprintf('<span class="product__price product__price--old">Старая цена: %s<span class="ruble">Р</span></span>', $item->old_price);
+                        }
+                        ?>
+                        <span class="product__price">Цена: <?= $item->new_price ?><span class="ruble">Р</span></span>
+                        <select class="product__size">
+                            <option>36 euro</option>
+                            <option>37 euro</option>
+                            <option>38 euro</option>
+                            <option>39 euro</option>
+                            <option>40 euro</option>
+                            <option>41 euro</option>
+                            <option>42 euro</option>
+                        </select>
+                        <a class="btn btn--ebn btn--block btn--red" href="">Заказать по акции</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</li>
+</div>
