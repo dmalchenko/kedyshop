9569bff032f1fcb9e5854f3250cb7e83f3c8e100
Nick Prokopenko <drnickopol@gmail.com>
2018-06-29 19:47:56 +0300

    Cart working

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -40,10 +40,10 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
             <path d="M146.069,320.369h268.1c30.4,0,55.2-24.8,55.2-55.2v-112.8c0-0.1,0-0.3,0-0.4c0-0.3,0-0.5,0-0.8c0-0.2,0-0.4-0.1-0.6   c0-0.2-0.1-0.5-0.1-0.7s-0.1-0.4-0.1-0.6c-0.1-0.2-0.1-0.4-0.2-0.7c-0.1-0.2-0.1-0.4-0.2-0.6c-0.1-0.2-0.1-0.4-0.2-0.6   c-0.1-0.2-0.2-0.4-0.3-0.7c-0.1-0.2-0.2-0.4-0.3-0.5c-0.1-0.2-0.2-0.4-0.3-0.6c-0.1-0.2-0.2-0.3-0.3-0.5c-0.1-0.2-0.3-0.4-0.4-0.6   c-0.1-0.2-0.2-0.3-0.4-0.5c-0.1-0.2-0.3-0.3-0.4-0.5s-0.3-0.3-0.4-0.5s-0.3-0.3-0.4-0.4c-0.2-0.2-0.3-0.3-0.5-0.5   c-0.2-0.1-0.3-0.3-0.5-0.4c-0.2-0.1-0.4-0.3-0.6-0.4c-0.2-0.1-0.3-0.2-0.5-0.3s-0.4-0.2-0.6-0.4c-0.2-0.1-0.4-0.2-0.6-0.3   s-0.4-0.2-0.6-0.3s-0.4-0.2-0.6-0.3s-0.4-0.1-0.6-0.2c-0.2-0.1-0.5-0.2-0.7-0.2s-0.4-0.1-0.5-0.1c-0.3-0.1-0.5-0.1-0.8-0.1   c-0.1,0-0.2-0.1-0.4-0.1l-339.8-46.9v-47.4c0-0.5,0-1-0.1-1.4c0-0.1,0-0.2-0.1-0.4c0-0.3-0.1-0.6-0.1-0.9c-0.1-0.3-0.1-0.5-0.2-0.8   c0-0.2-0.1-0.3-0.1-0.5c-0.1-0.3-0.2-0.6-0.3-0.9c0-0.1-0.1-0.3-0.1-0.4c-0.1-0.3-0.2-0.5-0.4-0.8c-0.1-0.1-0.1-0.3-0.2-0.4   c-0.1-0.2-0.2-0.4-0.4-0.6c-0.1-0.2-0.2-0.3-0.3-0.5s-0.2-0.3-0.3-0.5s-0.3-0.4-0.4-0.6c-0.1-0.1-0.2-0.2-0.3-0.3   c-0.2-0.2-0.4-0.4-0.6-0.6c-0.1-0.1-0.2-0.2-0.3-0.3c-0.2-0.2-0.4-0.4-0.7-0.6c-0.1-0.1-0.3-0.2-0.4-0.3c-0.2-0.2-0.4-0.3-0.6-0.5   c-0.3-0.2-0.6-0.4-0.8-0.5c-0.1-0.1-0.2-0.1-0.3-0.2c-0.4-0.2-0.9-0.4-1.3-0.6l-73.7-31c-6.9-2.9-14.8,0.3-17.7,7.2   s0.3,14.8,7.2,17.7l65.4,27.6v61.2v9.7v74.4v66.5v84c0,28,21,51.2,48.1,54.7c-4.9,8.2-7.8,17.8-7.8,28c0,30.1,24.5,54.5,54.5,54.5   s54.5-24.5,54.5-54.5c0-10-2.7-19.5-7.5-27.5h121.4c-4.8,8.1-7.5,17.5-7.5,27.5c0,30.1,24.5,54.5,54.5,54.5s54.5-24.5,54.5-54.5   s-24.5-54.5-54.5-54.5h-255c-15.6,0-28.2-12.7-28.2-28.2v-36.6C126.069,317.569,135.769,320.369,146.069,320.369z M213.269,431.969   c0,15.2-12.4,27.5-27.5,27.5s-27.5-12.4-27.5-27.5s12.4-27.5,27.5-27.5S213.269,416.769,213.269,431.969z M428.669,431.969   c0,15.2-12.4,27.5-27.5,27.5s-27.5-12.4-27.5-27.5s12.4-27.5,27.5-27.5S428.669,416.769,428.669,431.969z M414.169,293.369h-268.1   c-15.6,0-28.2-12.7-28.2-28.2v-66.5v-74.4v-5l324.5,44.7v101.1C442.369,280.769,429.669,293.369,414.169,293.369z"/>
         </svg>
         <span class="header-informer__text">Корзина</span>
-        <span class="header-informer__counter">(0)</span>
+        <span class="header-informer__counter">0</span>
     </button>
 
-    <modal v-if="showModal" @close="showModal = false" v-cloak>
+    <modal v-show="showModal" @close="showModal = false" v-cloak>
         <h3 slot="header">Корзина</h3>
     </modal>
 
@@ -152,7 +152,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
                     <input class="cart-table__input" type="text" :value="product.count">
                 </div>
                 <div class="cart-table__cell">
-                    <span>${{ getProductTotalPrice(product) }}</span>
+                    <span>{{ getProductTotalPrice(product) }}</span>
                     <span class="ruble">Р</span>
                 </div>
                 <div class="cart-table__cell">
@@ -170,7 +170,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
                     Итого:
                 </div>
                 <div class="cart-table__cell">
-                    <span>0</span>
+                    <span>{{ getProductsTotalPrice() }}</span>
                     <span class="ruble">Р</span>
                 </div>
                 <div class="cart-table__cell">
@@ -234,7 +234,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
         template: '#template-vss-cart',
         props: {
             type: String,
-//            eventable: Boolean
+            eventable: Boolean
         },
         name: 'vss-cart',
         data: function() {
@@ -263,6 +263,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
                 var self          = this;
                 var productNumber = self.findProductNumber(product);
                 if (productNumber === undefined) {
+                    console.log(1);
                     self.data.products.push({
                         id: product.id,
                         title: product.title,
@@ -272,6 +273,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
                     });
                 }
                 else {
+                    console.log(2);
                     self.data.products[productNumber].count += 1;
                 }
                 this.setVssCart();
@@ -292,6 +294,11 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
                 }
             },
             increaseProduct: function(product) {
+                var productNumber = this.findProductNumber(product);
+                this.data.products[productNumber].count++;
+                this.setVssCart();
+            },
+            setProduct: function(product, value) {
                 var productNumber = this.findProductNumber(product);
                 this.data.products[productNumber].count++;
                 this.setVssCart();
@@ -322,7 +329,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
                     price += this.data.products[i].count * this.data.products[i].price;
                 }
 
-                return price.toFixed(2);
+                return price.toFixed(0);
             },
             getProductTotalPrice: function(product) {
                 var productNumber = this.findProductNumber(product);
@@ -335,6 +342,7 @@ $isContacts = $route == 'site/contacts' ? 'main-menu__list-item--active' : '';
             self.initVssCart();
             vssCartBus.$on('vss-cart-add-product', function(product) {
 //                if (self.eventable) {
+                console.log(3);
                     self.addProduct(product);
 //                }
             });
